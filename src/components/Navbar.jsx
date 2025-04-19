3d737147245b6e51f425b4d547db8e1233a72d95
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-18 20:30:15 -0500

    se agregan botones de carrito, usuario y buscar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,37 @@
 import { memo } from "react"
 import { useNavigate } from "react-router-dom"
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Badge } from "@mui/material";
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import logo from "/src/static/img/logo.svg"
+import { Stack } from "@mui/material";
+import useStore from "/src/hooks/useStore";
 
 const Navbar = () => {
   const navigate = useNavigate()
 
+  const {order} = useStore()
+
   return (
     <nav className="sticky top-0 z-50 w-full backdrop-blur-sm bg-white/70 border-b border-zinc-200 shadow-sm transition-all">
-      <div className="container mx-auto flex items-center justify-between px-6 py-2">
+      <div className="container mx-auto flex items-center justify-between  py-2">
         <LogoStore
           action={() => {
             navigate('/store')
           }}
           logo={logo}
+
         />
         {/* Aquí podrías poner botones, links, etc */}
+        <Stack direction="row" spacing={2}>
+            <SearchOutlinedIcon sx={{ cursor: 'pointer', fontSize: 30, color: 'grey.700' }} />
+            <PersonOutlinedIcon sx={{ cursor: 'pointer', fontSize: 30, color: 'grey.700' }} />
+            <Badge badgeContent={order?.length} color="primary" overlap="circular">
+                <ShoppingCartOutlinedIcon sx={{ cursor: 'pointer', fontSize: 30, color: 'grey.700' }} />
+            </Badge>
+        </Stack>
+
       </div>
     </nav>
   )
