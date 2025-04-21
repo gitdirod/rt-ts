231dbe095e9d84680d9670df039bae6cb8f599aa
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-21 11:36:28 -0500

    se usa la etiqueca form desde un comoponente BOX

--- src/components/store/navbar/LoginModal.jsx
+++ src/components/store/navbar/LoginModal.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, TextField, Alert, Stack } from '@mui/material'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import PasswordIcon from '@mui/icons-material/Password';
 import { useAuth } from '/src/hooks/useAuth';
+
+import GppBadOutlinedIcon from '@mui/icons-material/GppBadOutlined';
 
 export default function LoginModal() {
 
@@ -12,40 +14,59 @@ export default function LoginModal() {
   const { login, errores } = useAuth({ middleware: 'guest', urlLogin:'/'});
 
   const handleSubmit = async (e) => {
-    // e.preventDefault(); 
+    e.preventDefault(); 
+
+    if (!email || !password) {
+      return;
+    }
+
     await login({ email, password });
   };
 
-  console.log(errores)
-
   return (
-    <Box mt={2}>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', mb: 2 }}>
-        <AlternateEmailIcon sx={{ color: 'action.active', mr: 1 }} />
-        <TextField 
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          label="Correo" 
-          variant="standard" 
-          type="email" 
-          fullWidth 
-        />
+    <Box component="form" onSubmit={handleSubmit} mt={2}>
+      <Box sx={{ display: 'flex', flexDirection: 'column',  mb: 2, gap:1 }}>
+        <Stack direction="row">
+          <AlternateEmailIcon sx={{ color: 'action.active', mr: 1 }} />
+          <TextField 
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            label="Correo" 
+            variant="standard" 
+            type="email" 
+            fullWidth 
+          />
+        </Stack>
+        {errores?.email && (
+            <Alert icon={<GppBadOutlinedIcon fontSize="inherit" />} severity="error">
+            {errores?.email}
+            </Alert>
+        )}
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', mb: 2 }}>
-        <PasswordIcon sx={{ color: 'action.active', mr: 1 }} />
-        <TextField 
-        value={password}
-        onChange={(event)=>setPassword(event.target.value)}
-        label="Contraseña" 
-        variant="standard" 
-        type="password" 
-        fullWidth 
-      />
+      <Box sx={{ display: 'flex', flexDirection:'column', mb: 2, gap:1 }}>
+        <Stack direction="row">
+          <PasswordIcon sx={{ color: 'action.active', mr: 1 }} />
+          <TextField 
+            value={password}
+            onChange={(event)=>setPassword(event.target.value)}
+            label="Contraseña" 
+            variant="standard" 
+            type="password" 
+            fullWidth 
+          />
+        </Stack>
+        {errores?.password && (
+            <Alert icon={<GppBadOutlinedIcon fontSize="inherit" />} severity="error">
+            {errores?.password}
+            </Alert>
+        )}
+
       </Box>
-      <Button 
-        variant="contained" 
+      <Button
+        type="submit"
+        variant="contained"
+        disabled={!email || !password} // o loading si lo tienes
         sx={{ width: 1, fontWeight: 'bold', textTransform: 'none' }}
-        onClick={handleSubmit}
       >
         Iniciar sesión
       </Button>
