b149d24de01e92e26493814a32417bb5fffb0723
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-22 11:19:07 -0500

    se agrega titulo con el monto a pagar

--- src/components/store/cart/StepTwoPaymentMethod.jsx
+++ src/components/store/cart/StepTwoPaymentMethod.jsx
@@ -35,53 +35,65 @@ export default function StepTwoPaymentMethod() {
   return (
     <Container maxWidth="md">
       <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
+            <Typography
+                variant="h5"
+                color="primary"
+                fullWidth
+                textAlign="center"
+                sx={{ fontWeight: 'bold', mb: 2 }}
+            >
+                Total a pagar: {formatearDinero(total)}
+            </Typography>
         <FormControl component="fieldset" fullWidth>
-          <FormLabel component="legend" sx={{ mb: 2, fontWeight: 'bold' }}>
-            Selecciona un método de pago
-          </FormLabel>
-          <RadioGroup
-            name="payment-method"
-            value={paymentMethod}
-            onChange={handlePaymentChange}
-          >
-            <FormControlLabel
-              value="transfer"
-              control={<Radio />}
-              label={`Transferencia bancaria ${formatearDinero(total)}`}
-            />
-            <FormControlLabel
-              value="card"
-              control={<Radio />}
-              label="Tarjeta de débito o crédito (próximamente)"
-              disabled
-            />
-          </RadioGroup>
+            
 
-          {paymentMethod === 'transfer' && (
-            <>
-              <Box sx={{ mt: 4 }}>
-                {banks.map((bank, index) => (
-                  <BankDescription key={index} bank={bank} />
-                ))}
-              </Box>
+            <FormLabel component="legend" sx={{ mb: 2, fontWeight: 'bold' }}>
+                Selecciona un método de pago
+            </FormLabel>
+            
+            <RadioGroup
+                name="payment-method"
+                value={paymentMethod}
+                onChange={handlePaymentChange}
+            >
+                <FormControlLabel
+                value="transfer"
+                control={<Radio />}
+                label={`Transferencia bancaria ${formatearDinero(total)}`}
+                />
+                <FormControlLabel
+                value="card"
+                control={<Radio />}
+                label="Tarjeta de débito o crédito (próximamente)"
+                disabled
+                />
+            </RadioGroup>
 
-              <Box sx={{ mt: 3 }}>
-                <Typography variant="body1" sx={{ mb: 1 }}>
-                  Subir comprobante de transferencia:
-                </Typography>
-                <Button
-                  variant="outlined"
-                  component="label"
-                  startIcon={<UploadIcon />}
-                >
-                  Seleccionar imagen
-                  <Input
-                    type="file"
-                    accept="image/*"
-                    hidden
-                    onChange={handleFileUpload}
-                  />
-                </Button>
+            {paymentMethod === 'transfer' && (
+                <>
+                <Box sx={{ mt: 4 }}>
+                    {banks.map((bank, index) => (
+                    <BankDescription key={index} bank={bank} />
+                    ))}
+                </Box>
+
+                <Box sx={{ mt: 3 }}>
+                    <Typography variant="body1" sx={{ mb: 1 }}>
+                    Subir comprobante de transferencia:
+                    </Typography>
+                    <Button
+                    variant="outlined"
+                    component="label"
+                    startIcon={<UploadIcon />}
+                    >
+                    Seleccionar imagen
+                    <Input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleFileUpload}
+                    />
+                    </Button>
               </Box>
             </>
           )}
