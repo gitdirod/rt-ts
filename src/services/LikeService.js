d40441a12e18fe500fa0ba53c6106108cb0cf101
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-23 09:48:47 -0500

    se agrga al servicio de likes el metodo useAllLikes, el cual permite traer todos los likes sin paginacion

--- src/services/LikeService.js
+++ src/services/LikeService.js
@@ -2,19 +2,17 @@ import useSWR from 'swr';
 import { fetchData, swrConfig} from '/src/utils/fetchData';
 import BACKEND from '/src/data/backend';
 
-export const ProductService = {
-    useProducts: ({ page, perPage, name, code, group_id, categories, types, sortField, sortOrder }) => {
-        // Construcción de la URL con los parámetros
-        const url = `${BACKEND.PRODUCTS.KEY}?page=${page}&perPage=${perPage}&name=${name}&code=${code}&group_id=${group_id}&categories=${categories}&types=${types}&sortField=${sortField}&sortOrder=${sortOrder}`;
+export const LikeService = {
+    useAllLikes: () => {
+        const url = BACKEND.LIKES.KEY;
 
-        // Uso de SWR para obtener productos
+        // Uso de SWR con fetchData como fetcher
         const { data, error, mutate } = useSWR(
             url,
-            fetchData,
+            fetchData, // Usando la función genérica
             swrConfig
         );
 
-        // Retornar los datos y funciones de SWR
         return {
             data: data?.data || [],
             totalRecords: data?.meta?.total || 0,
@@ -22,46 +20,6 @@ export const ProductService = {
             error,
             mutate
         };
-    },
-    usePublicProducts: ({ page, perPage, name, code, group_id, categories, types, sortField, sortOrder }) => {
-        // Construcción de la URL con los parámetros
-        const url = `${BACKEND.PUBLIC_PRODUCTS.KEY}?page=${page}&perPage=${perPage}&name=${name}&code=${code}&group_id=${group_id}&categories=${categories}&types=${types}&sortField=${sortField}&sortOrder=${sortOrder}`;
-
-        // Uso de SWR para obtener productos
-        const { data, error, mutate } = useSWR(
-            url,
-            fetchData,
-            swrConfig
-        );
-
-        // Retornar los datos y funciones de SWR
-        return {
-            data: data?.data || [],
-            totalRecords: data?.meta?.total || 0,
-            loading: !data && !error,
-            error,
-            mutate
-        };
-    },
-    useNoSuggestedProducts: ({ page, perPage, name, code, sortField, sortOrder }) => {
-        // Construcción de la URL con los parámetros
-        const url = `${BACKEND.NO_SUGGESTED_PRODUCTS.KEY}?page=${page}&perPage=${perPage}&name=${name}&code=${code}&sortField=${sortField}&sortOrder=${sortOrder}`;
-
-        // Uso de SWR para obtener productos
-        const { data, error, mutate } = useSWR(
-            url,
-            fetchData,
-            swrConfig
-        );
-
-        // Retornar los datos y funciones de SWR
-        return {
-            data: data?.data || [],
-            totalRecords: data?.meta?.total || 0,
-            loading: !data && !error,
-            error,
-            mutate
-        };
-    },
+    }
 
 };
