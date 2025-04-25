b3836c4c131aaa5bbd78a6473aeaae8a525e035e
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-24 20:46:18 -0500

    se eliminn archivos no necesarios

---
+++ src/data/urlsBackend copy.js
@@ -0,0 +1,7 @@
+export const urlsBackend = {
+    PRODUCT_IMAGE: import.meta.env.VITE_API_URL +'/products/',
+    CATEGORY: import.meta.env.VITE_API_URL +'/categories/',
+    ICON: import.meta.env.VITE_API_URL +'/iconos/',
+    MEMORY:import.meta.env.VITE_API_URL + '/memories/',
+    SOLD_ORDER_PAYMENT_IMAGE:import.meta.env.VITE_API_URL + '/sold_order_payment_image/',
+  };
