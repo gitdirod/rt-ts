83145e488e913ff15bf411455108ce2de297f10c
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-04-27 00:57:50 -0500

    se factorizan las funciones en IndexProduct.jsx

--- src/components/admin/product/ProductFilters.jsx
+++ src/components/admin/product/ProductFilters.jsx
@@ -1,24 +1,57 @@
-import { Box, TextField, Stack } from '@mui/material';
+import { Box, Stack, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 
-export default function ProductFilters({ filterName, filterCode, onFilterNameChange, onFilterCodeChange }) {
+export default function ProductFilters({setDebouncedFilterName, setDebouncedFilterCode}) {
+
+  const [filterName, setFilterName] = useState('')
+  const [filterCode, setFilterCode] = useState('')
+
+
+  // Debounce para el nombre
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedFilterName(filterName);
+    }, 500); // espera 500 ms
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [filterName]);
+
+  // Debounce para el código
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedFilterCode(filterCode);
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [filterCode]);
+
+
   return (
-    <Stack direction="row" gap={2}>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth: 200 }}>
+    <Stack direction="row" gap={1} alignItems="center" >
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth:200 }}>
         <TextField
           label="Buscar por nombre"
+          id="outlined-size-small"
           size="small"
-          value={filterName}
-          onChange={(e) => onFilterNameChange(e.target.value)}
+          onChange={(event) => {
+            setFilterName(event.target.value);
+          }}
         />
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth: 200 }}>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth:200}}>
         <TextField
           label="Buscar por código"
+          id="outlined-size-small"
           size="small"
-          value={filterCode}
-          onChange={(e) => onFilterCodeChange(e.target.value)}
+          onChange={(event) => {
+            setFilterCode(event.target.value);
+          }}
         />
       </Box>
     </Stack>
-  );
+  )
 }
--- src/views/admin/IndexProduct.jsx
+++ src/views/admin/IndexProduct.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import {
-  TextField,
   Box,
   Typography,
   Chip
@@ -13,27 +12,28 @@ import { ProductService } from '/src/services/ProductService';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import ProductTable from '/src/components/admin/product/ProductTable';
 import ModalStoreUpdateProduct from '/src/components/admin/product/ModalStoreUpdateProduct';
+import ProductFilters from '/src/components/admin/product/ProductFilters';
 
 export default function MiTablaConPaginacion() {
  
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(25);
-  const [filterName, setFilterName] = useState('')
-  const [filterCode, setFilterCode] = useState('')
-  const [debouncedFilterName, setDebouncedFilterName] = useState('');
-  const [debouncedFilterCode, setDebouncedFilterCode] = useState('');
-
+  // Variables control ModalStoreUpdateProduct
   const [selectedProduct, setSelectedProduct] = useState({})
-
-
   const [edit, setEdit] = useState(false);
-
   const handleCloseEdit = () => setEdit(false);
   const handleEdit = (product) => {
     setSelectedProduct(product)
     setEdit(true)
   };
 
+
+  //variables filtrado
+  const [debouncedFilterName, setDebouncedFilterName] = useState('');
+  const [debouncedFilterCode, setDebouncedFilterCode] = useState('');
+
+
+  // Variables paginación
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
   
   const { data: products, totalRecords, loading, mutate } = ProductService.useProducts({
     page: page + 1,
@@ -59,31 +59,6 @@ export default function MiTablaConPaginacion() {
   const totalPages = Math.ceil((totalRecords || 0) / rowsPerPage);
   const safePage = (totalPages === 0) ? 0 : (page >= totalPages ? totalPages - 1 : page);
   
-
-  // Debounce para el nombre
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedFilterName(filterName);
-    }, 500); // espera 500 ms
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [filterName]);
-
-  // Debounce para el código
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedFilterCode(filterCode);
-    }, 500);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [filterCode]);
-
-
-
   useEffect(() => {
     const totalPages = Math.ceil((totalRecords || 0) / rowsPerPage);
     if (page >= totalPages && totalPages > 0) {
@@ -114,28 +89,12 @@ export default function MiTablaConPaginacion() {
           </Typography>
           <Chip label={totalRecords || 0} color="primary" />
         </Stack>
-        <Stack direction="row" gap={1} alignItems="center" >
-          <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth:200 }}>
-            <TextField
-              label="Buscar por nombre"
-              id="outlined-size-small"
-              size="small"
-              onChange={(event) => {
-                setFilterName(event.target.value);
-              }}
-            />
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'flex-end', maxWidth:200}}>
-            <TextField
-              label="Buscar por código"
-              id="outlined-size-small"
-              size="small"
-              onChange={(event) => {
-                setFilterCode(event.target.value);
-              }}
-            />
-          </Box>
-        </Stack>
+
+        <ProductFilters
+          setDebouncedFilterName={setDebouncedFilterName}
+          setDebouncedFilterCode={setDebouncedFilterCode}
+        />
+
         <Button variant="contained" color="primary" startIcon={<AddCircleOutlineIcon />} 
           onClick={() => {
           handleEdit(null); // por ejemplo, abrir modal de vista
@@ -154,8 +113,6 @@ export default function MiTablaConPaginacion() {
         handleChangeRowsPerPage={handleChangeRowsPerPage}
         handleEdit={handleEdit}
       />
-
-    {/* </div> */}
     </Box>
   );
 }
