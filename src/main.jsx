d0a32f3f266d5c6f8ce364c7124ffe86b1269ac4
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-05-12 21:34:59 -0500

    se elimina tailwind del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import router from './router'
 import theme from './theme'
-import './index.css'
-import './static/fonts/fonts.css';
+// import './index.css'
+// import './static/fonts/fonts.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
