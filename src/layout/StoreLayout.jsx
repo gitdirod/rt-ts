ed9fdbca04ac250aabf3b5da830e81d829ccc274
Diego Rodriguez Git <git@dirod-ecuador.com>
2025-06-06 12:27:49 -0500

    se remueven clases de tailwind en StoreLayout

--- src/layout/StoreLayout.jsx
+++ src/layout/StoreLayout.jsx
@@ -1,23 +1,35 @@
 import { memo } from "react";
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 import CartDrawer from "/src/components/CartDrawer";
 import StoreNavbar from "../components/store/navbar/StoreNavbar";
+import { Box } from "@mui/material";
 
 const StoreLayout = () => {
-
-
   return (
-    <div className="relative cursor-default min-h-screen ">
-      <StoreNavbar/>
+    <Box
+      position="relative"
+      sx={{
+        cursor: "default",
+        minHeight: "100vh",
+      }}
+    >
+      <StoreNavbar />
 
-      {/*  Aquí le das margen superior equivalente a la altura del navbar */}
-      <div className="flex flex-col  transition-all">
+      {/* Margen superior ya no es necesario si StoreNavbar es sticky */}
+      <Box
+        display="flex"
+        flexDirection="column"
+        sx={{
+          transition: "all 0.3s ease",
+        }}
+      >
         <Outlet />
-      </div>
+      </Box>
 
       <CartDrawer />
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
-export default memo(StoreLayout)
+export default memo(StoreLayout);
+
